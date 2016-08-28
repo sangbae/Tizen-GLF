550920f0f7bcccd62e96da142ec6a3bc32324738
Sangbae Lee <sangbae90.lee@samsung.com>
2016-08-28 13:13:14 +0900

    test

--- tizen_common_artik_build.sh
+++ tizen_common_artik_build.sh
@@ -37,17 +37,13 @@ read userid
 # Step1: working environment
 work_dir=./tizen_common_artik
 base_dir=./tizen_base
-
 if [ -f $work_dir ]; then
 	echo "work directory exists already [$work_dir]" else 
 	mkdir -p $work_dir
 	echo "work directory for Tizen-common for ARTIK-10 = $work_dir"
-fi 
-if [ -f $base_dir ]; then
+fi if [ -f $base_dir ]; then
 	echo "base directory exists already [$base_dir]"
-else 
-	mkdir -p $base_dir
-	echo "base directory for Tizen-common for ARTIK-10 = $base_dir"
+else mkdir -p $base_dir echo "base directory for Tizen-common for ARTIK-10 = $base_dir"
 fi
 
 # Step1: repo sync
@@ -57,24 +53,40 @@ repo init -u $manifest_url -b tizen -m common.xml
 #repo init -u ssh://$userid@review.tizen.org:29418/scm/manifest -b tizen -m common.xml
 
 
-cp ../Tizen-GLF/tizen-common-artik_20160721.17_platform.xml .repo/manifests/common/
-cp ../Tizen-GLF/common.xml .repo/manifests/
+cp ../Tizen-GLF/tizen-common-artik_20160721.17_platform.xml .repo/manifests/common/ cp ../Tizen-GLF/common.xml .repo/manifests/
 
 repo sync -f -q
 cd ..
 
 # Step 2: Tizen Base-packages download
-cd $base_dir
-wget --directory-prefix=./ --mirror --reject index.html* -r -nH --no-parent --cut-dirs=8 http://download.tizen.org/snapshots/tizen/base/latest/repos/arm/packages
-cd ..
+base_download()
+{
+	cd $base_dir
+	wget --directory-prefix=./ --mirror --reject index.html* -r -nH --no-parent --cut-dirs=8 http://download.tizen.org/snapshots/tizen/base/latest/repos/arm/packages
+	cd ..
+}
+
+if [ -d $base_dir ]; then
+	echo "$base_dir exist already"
+	echo "do you want to download base packages ?"
+	echo " [Y]es? >"
+	read yorn
+	if [ yorn -eq "Y" ]; then 
+		sudo \rm -r $base_dir
+		base_download()
+	fi
+else 
+	baes_download()
+fi
+
 
 # Step 3: build Tizen-Common locally
 cd $work_dir
 cp ../Tizen_GLF/gbs_conf_artik_local_full_build  ./.gbs.conf
-gbs build -A armv7l 
+time gbs build -A armv7l --baselibs --clean-once 
 
 
 # Step 4: create boot image
-sudo mic cr auto ../Tizen-GLF/common-boot-armv7l-artik10.ks --logfile=./log -o ./mic_images --tmpfs
+time sudo mic cr auto ../Tizen-GLF/common-boot-armv7l-artik10.ks --logfile=./log -o ./mic_images --tmpfs
 # Step 3: create platform image 
-sudo mic cr auto ../Tizen-GLF/common-artik-platform-armv7l.ks --logfile=./log -o ./mic_images --tmpfs
+time sudo mic cr auto ../Tizen-GLF/common-artik-platform-armv7l.ks --logfile=./log -o ./mic_images --tmpfs
